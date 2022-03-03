07d983b10034962bfdf2a3dd2bf8dec699fa584f
dollarortwo <jleong001@gmail.com>
2022-03-03 15:06:34 -0800

    added function definitions

---
+++ Number-Swap.cpp
@@ -0,0 +1,18 @@
+// Program to demostrate call-by-reference parameters.
+
+#include<iostream>
+using namespace std;
+
+void get_numbers(int& input1, int& input2);
+// Reads two integers from the keyboard.
+
+void swap_numbers(int& variable1, int& variable2);
+// Interchanges the values of variable1 and variable2
+
+void show_results(int output1, int output2);
+// Shows the values of variable1 and variable2, in that order.
+
+int main()
+{
+ return 0;
+}
